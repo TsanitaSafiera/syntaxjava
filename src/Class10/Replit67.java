078ec2bacf9c171321eecc9a6663bd74144d57e1
TsanitaSafiera <wiekecahyadi@gmail.com>
2023-04-15 01:34:14 -0400

    update

---
+++ src/Class10/Replit67.java
@@ -0,0 +1,21 @@
+package Class10;
+
+public class Replit67 {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <=5; i++) {
+            for (int j = 0; j < 10; j++) {
+
+                System.out.print(i+(j)*i+" ");
+
+
+
+            }
+            System.out.println();
+
+
+        }
+
+    }
+}
